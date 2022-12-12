8ea9eae075b7a85816bd25f79615735c6371788d
Neil Sullivan <ac30delay@gmail.com>
2022-12-12 10:01:08 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for ( let item of clay) {
+   const mug = `${item} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
